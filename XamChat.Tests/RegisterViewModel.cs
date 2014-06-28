1e95eef89b2119d57b3522d9070cb124a08dfc1b
Phil Hack <phil@philhack.com>
2014-06-28 21:06:04 +0100

    added first RegisterViewModel test

---
+++ XamChat.Tests/RegisterViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+using XamChat.Core;
+
+namespace XamChat.Tests {
+	[TestFixture]
+	public class RegisterViewModelTests {
+		ISettings settings;
+		RegisterViewModel registerViewModel;
+
+		[SetUp]
+		public void SetUp() {
+			settings = ServiceContainer.Resolve<ISettings>();
+			registerViewModel = new RegisterViewModel();
+		}
+
+		[Test]
+		public void RegisterSuccessfully() {
+			registerViewModel.Username = "testuser";
+			registerViewModel.Password = "password";
+			registerViewModel.ConfirmPassword = "password";
+
+			registerViewModel.Register().Wait();
+
+			Assert.That(settings.User, Is.Not.Null);
+		}
+	}
+}
+
