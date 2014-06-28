8b8bdb230d9a0dfba806373b92b4f93bc55abe43
Phil Hack <phil@philhack.com>
2014-06-28 18:04:16 +0100

    added MessageViewModel

---
+++ XamChat.Core/ViewModels/MessageViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace XamChat.Core {
+	public class MessageViewModdel : BaseViewModel {
+		public Conversation[] Conversations { get; set; }
+
+		public Conversation Conversation { get; set; }
+
+		public Message[] Messages { get; set; }
+
+		public string Text { get; set; }
+
+		public async Task GetConversations() {
+			if (settings.User = null)
+				throw new Exception("Not logged in.");
+
+			IsBusy = true;
+			try {
+				Conversations = await service.GetConversations(settings.User.Id);
+			} finally {
+				IsBusy = false;
+			}
+		}
+
+		public async Task GetMessages() {
+			if (Conversations == null)
+				throw new Exception("No conversation.");
+
+			IsBusy = true;
+
+			try {
+				Messages = await service.GetMessages(Conversation.Id);
+			} finally {
+				IsBusy = false;
+			}
+		}
+
+		public async Task SendMessage() {
+			if (settings.User == null)
+				throw new Exception("Not logged in.");
+			if (Conversation == null)
+				throw new Exception("No conversation.");
+			if (string.IsNullOrEmpty(Text))
+				throw new Exception("Message is blank.");
+
+			IsBusy = true;
+
+			try {
+				var message = await service.SendMessage(
+					              new Message {
+						UserId = settings.User.Id,
+						ConversationId = Conversation.Id,
+						Text = Text
+					}
+				              );
+
+				// update our local list of messares
+				var messages = new List<Message>();
+				if (Messages != null)
+					messages.AddRange(Messages);
+				messages.Add(message);
+
+				Messages = messages.ToArray();
+
+			} finally {
+				IsBusy = false;
+			}
+		}
+	}
+}
