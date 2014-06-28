fcf21ddb3dc1d8bd7ec26d7ef7cac8d9a0382b9f
Phil Hack <phil@philhack.com>
2014-06-28 15:59:19 +0100

    added friend view model

---
+++ XamChat.Core/ViewModels/FriendViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace XamChat.Core {
+	public class FriendViewModel : BaseViewModel {
+		public User[] Friends { get; private set; }
+
+		public string Username { get; set; }
+
+		public async Task GetFriends() {
+			if (settings.User == null) {
+				throw new NotImplementedException("Not logged in");
+			}
+
+			IsBusy = true;
+
+			try {
+				Friends = await service.GetFriends(settings.User.Id);
+			} finally {
+				IsBusy = false;
+			}
+		}
+
+		public async Task AddFriend() {
+			if (settings.User == null)
+				throw new Exception("Not logged in.");
+			if (string.IsNullOrEmpty(Username))
+				throw new Exception("Username is blank.");
+
+			IsBusy = true;
+
+			try {
+				var friend = await service.AddFriend(settings.User.Id, Username);
+
+				// update our local list of friends
+				var friends = new List<User>();
+				if (Friends != null)
+					friends.AddRange(Friends);
+				friends.Add(friend);
+
+				Friends = friends.OrderBy(f => f.Username).ToArray();
+			} finally {
+				IsBusy = false;
+			}
+		}
+	}
+}
+
