03ab86364c1eec956798d194bc0beb163d8843ff
Phil Hack <phil@philhack.com>
2014-06-28 21:53:59 +0100

    added loginController

---
+++ XamChat.iOS/Controllers/LoginController.cs
@@ -0,0 +1,14 @@
+using System;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using System.CodeDom.Compiler;
+
+namespace XamChat.iOS
+{
+	partial class LoginController : UINavigationItem
+	{
+		public LoginController (IntPtr handle) : base (handle)
+		{
+		}
+	}
+}
